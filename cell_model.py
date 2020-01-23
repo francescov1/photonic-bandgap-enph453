6aafc198fbd030d3737395bafedb09e12b20b5bf
Francesco Virga <francescod.virga@gmail.com>
2020-01-23 17:05:56 -0500

    Finish model

---
+++ cell_model.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+class CellModel:
+
+    def __init__(self, l_75, l_50):
+        self.l_1 = l_75
+        self.l_2 = l_50
+        self.Z_1 = 75
+        self.Z_2 = 50
+        self.v = 0.66 * 3.0 * (10**8) # 2/3 * speed of light
+
+    def get_band_gap_frequency(self):
+        return self.v / (2 * (self.l_1 + self.l_2))
+
+    def set_transfer_mats(self):
+
+        def calc_transfer_mat(Z_a, Z_b):
+            r_ab = (Z_a - Z_b)/(Z_a + Z_b)
+            t_ab = (2 * Z_a)/(Z_a + Z_b)
+            # transfer matrix
+            # magnitudes at interface: [[a_2], [b_2]] = T_12 * [[a_1], [b_1]]
+            T_ab = np.multiply(1/t_ab, np.array([[1, r_ab], [r_ab, 1]]))
+            return T_ab
+
+        self.T_12 = calc_transfer_mat(self.Z_1, self.Z_2)
+        self.T_21 = calc_transfer_mat(self.Z_2, self.Z_1)
+
+    def set_phase_mats(self, f_rf):
+
+        eps = np.finfo(float).eps # epsilon
+
+        def calc_phase_mat(f, l, v):
+            phi = (2 * np.pi * f * l) / v
+
+            # P_i = [[e^(i*phi), 0], [0, e^(i*phi)]] - i in equation is complex i, not i in P_i
+            P = np.array([[eps**complex(0, phi), 0], [0, eps**complex(0, phi)]])
+            return P
+
+        self.P_1 = calc_phase_mat(f_rf, self.l_1, self.v)
+        self.P_2 = calc_phase_mat(f_rf, self.l_2, self.v)
+
+    def set_matrix_model(self):
+        # model unit cell of coaxial cable by multiply transfer and propagation matrices
+        # M_cell = T_21 * P_2 * T_12 * P_1
+        M_cell = np.matmul(self.T_21, self.P_2)
+        M_cell = np.matmul(M_cell, self.T_12)
+        M_cell = np.matmul(M_cell, self.P_1)
+
+        self.M_cell = M_cell
--- main.py
+++ main.py
@@ -1,76 +1,60 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from lengths import cells as cell_lengths, impurities as impurity_lengths
+from cell_model import CellModel
 
-############# Values #############
+def get_total_M(cells):
+    M_total = None
+    for cell in cells:
+        if (M_total is None):
+            M_total = cell.M_cell
+        else:
+            M_total = np.matmul(M_total, cell.M_cell)
 
-# NOTE: '1' denotes the 75 ohm wire, '2' denotes the 50 ohm wire
-
-c = 3.0 * (10**8) # speed of light
-eps = np.finfo(float).eps # epsilon
-
-num_cells = 2 # number of cells (combination of two wires)
-
-# TODO: measure
-l_1 = 1 # length of 75 ohm wire
-# TODO: measure
-l_2 = 1 # length of 50 ohm wire
-
-# Z_n = electrical impedance for n wire => replaces index of refraction n
-Z_1 = 75
-Z_2 = 50
-
-# phase_vel in wires
-v = v_1 = v_2 = 0.66 * c
-
-f_rf = 100 # MHz
-f_rf *= 1000000 # convert to Hz
-
-f_0 = v / (2 * (l_1 + l_2))
-print("Band gap frequency: " + str(f_0/1000000) + "MHz")
+    return M_total
 
 # get frequency response of total reflection
 def get_frequency_response(M_cell):
-    R = (abs(M_cell[1, 0])**2) / (abs(M_cell[0, 0])**2)
+    # this is what the textbook says
+    #R = (abs(M_cell[1, 0])**2) / (abs(M_cell[0, 0])**2)
+
+    # this looks a bit more accurate tho
+    R = (abs(M_cell[0, 1])**2) / (abs(M_cell[1, 0])**2)
     return R
 
-def get_transfer_mat(Z_a, Z_b):
-    r_ab = (Z_a - Z_b)/(Z_a + Z_b)
-    t_ab = (2 * Z_a)/(Z_a + Z_b)
+cell_lengths = cell_lengths[:2]
 
-    # transfer matrix
-    # magnitudes at interface: [[a_2], [b_2]] = T_12 * [[a_1], [b_1]]
+start_f = 50 # MHz
+stop_f = 850 # MHz
+num_points = 100
+frequencies = np.linspace(start=start_f*1000000, stop=stop_f*1000000, num=num_points)
 
-    T_ab = np.multiply(1/t_ab, np.array([[1, r_ab], [r_ab, 1]]))
-    return T_ab
+f_responses = []
+transmission = []
+for f_rf in frequencies:
+    cells = []
 
-def get_phase_mat(f_rf, l, v):
-    phi = (2 * np.pi * f_rf * l) / v
+    for cell_length in cell_lengths:
+        cell = CellModel(cell_length['l_75'], cell_length['l_50'])
 
-    # P_i = [[e^(i*phi), 0], [0, e^(i*phi)]] - i in equation is complex i, not i in P_i
-    P = np.array([[eps**complex(0, phi), 0], [0, eps**complex(0, phi)]])
-    return P
+        #band_gap_f = cell.get_band_gap_frequency()
+        #print("Band gap frequency: " + str(band_gap_f/1000000) + "MHz")
+        cell.set_transfer_mats()
+        cell.set_phase_mats(f_rf)
+        cell.set_matrix_model()
+        cells.append(cell)
 
-# for a specific f - we will need to generalize this
-# f_rf = frequency of rf signal
-def get_matrix_model(T_ab, T_ba, P_a, P_b):
-    # model unit cell of coaxial cable by multiply transfer and propagation matrices
-    # M_cell = T_21 * P_2 * T_12 * P_1
-    M_cell = np.matmul(T_ba, P_b)
-    M_cell = np.matmul(M_cell, T_ab)
-    M_cell = np.matmul(M_cell, P_a)
+    M_total = get_total_M(cells)
 
-    # for multiple cells
-    #M_cell = np.linalg.matrix_power(M_cell, num_cells)
-    return M_cell;
+    R = get_frequency_response(M_total)
+    f_responses.append(R)
+    transmission.append(1-R)
 
-T_12 = get_transfer_mat(Z_1, Z_2)
-T_21 = get_transfer_mat(Z_2, Z_1)
-P_1 = get_phase_mat(f_rf, l_1, v_1)
-P_2 = get_phase_mat(f_rf, l_2, v_2)
+#print(f_responses)
+plt.plot(frequencies, f_responses)
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Response")
+plt.show()
 
-M_cell = get_matrix_model(T_12, T_21, P_1, P_2)
-
-print(M_cell)
-R = get_frequency_response(M_cell)
-
-print("frequency response: " + str(R))
-print("transmission: " + str(1-R))
+# for multiple cells
+#M_cell = np.linalg.matrix_power(M_cell, num_cells)
