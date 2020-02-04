6388f5f104c089d711f4af5d41e52d1d72f74be1
Francesco Virga <francescod.virga@gmail.com>
2020-02-03 19:47:48 -0500

    Readd comments in main.py

--- main.py
+++ main.py
@@ -4,38 +4,42 @@ import fabry_perot as fb
 import impurity as imp
 import exp_data as ed
 
-# - to add impurity, uncomment the lines below and sepcify params
-# - impurity is always added to the end of the cell at specified position,
-#   expect for @ position -1, where its added at the very front
-# - can be "fp" (50ohm impurity - position gets set to middle of cells automatically) or
-#   "impure" (93ohm impurity, set @ position -1,0,1,2,3,4,5)
-
 #for all configurations
 n_cells = 6
 start_f = 50e6 # MHz
 stop_f = 850e6 # MHz
 n_points = 1000
 
-
 apparatus = ApparatusModel(n_cells, start_f, stop_f, n_points)
 
 #for pure configurations
 # ed.plot_exp_data(n_cells)
 
+# - to add impurity, uncomment the lines below and sepcify params
+# - impurity is always added to the end of the cell at specified position,
+#   expect for @ position -1, where its added at the very front
+# - can be one of two types:
+#     - "fp":
+#         - impedance: 50ohm
+#         - position: set to middle of cells automatically
+#         - number of cables: specify one of following: 2,4
+#     - "impure" (93ohm impurity, set @ position )
+#         - impedance: 93ohm
+#         - position: specify one of following: -1,0,1,2,3,4,5
+#         - number of cables: set to 1 automatically
+
 #for impure option
 # impurity_position = 3
 # impurity_type = "impure"
-# impurity_cable_length = 2
 # imp.plot_imp(impurity_position)
-# apparatus.add_impurity(type=impurity_type, cell_position=impurity_position,n_cables=impurity_cable_length)
+# apparatus.add_impurity(type=impurity_type, cell_position=impurity_position,n_cables=1)
 
 #for fp
 # n_fb_cells = 2 # can be 2 or 4
 # impurity_position = 0
-# impurity_cable_length = 2
 # impurity_type = "fp"
 # fb.plot_fb(n_fb_cells)
-# apparatus.add_impurity(type=impurity_type, cell_position=impurity_position,n_cables=impurity_cable_length)
+# apparatus.add_impurity(type=impurity_type, cell_position=impurity_position,n_cables=n_fb_cells)
 
 apparatus.calculate_response()
 
